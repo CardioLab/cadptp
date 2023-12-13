b51b9914e4fc31f84266ecfbfb5a5f71e5fd600f
Sameul Emil Schmidt <sschmidt@hst.aau.dk>
2023-12-13 11:25:40 +0100

    R code for CTA ptp models

---
+++ R/cadptp_cta.R
@@ -0,0 +1,91 @@
+#' The cadptp_cta calculates pre-test probability (PTP) of having obstructive coronary artery disease defined from CT scanning in patients with chronic coronary syndrome 
+#' 
+#' @description
+#' The cadptp_cta command calculates pre-test probability (PTP) of having obstructive coronary artery disease defined from CT scanning in patients with chronic
+#' coronary syndrome (symptoms suggestive of obstructive coronary artery disease) without previously documented coronary artery disease.
+#' The model is based on data from >40,000 patients and validated in >15.000 (ref).  When only age, gender and symptoms are used as
+#' input the basic PTP (ptp_basic) is calcualted.  If the number of risk factors are given the risk factor weighted clinical
+#' likelihood of CAD (ptp_rf) is also calculated.  Finally, if CACS is given a CACS weighted clinical likelihood (ptp_cacs) is
+#' estimated.
+#' 
+#' 
+#' @param sex gender, where male = 1 and female = 0
+#' @param age  Age in years
+#' @param symp_gr   Symptoms, where Non-typical angina=0, Atypical angina or dyspnoea=1 and Typical angina =2
+#' @param nb_rf  Number of risk factors (0-5) (Riskfactors: Family history of early CAD, Smoking, Dyslipidaemia, Hypertension or Diabetes)
+#' @param cacs  Coronary artery calcium score determined by the Agatston method
+#'
+#'
+#' @return ptp 
+#' @examples 
+#' The current example download a Synthetic dataset of patients with chronic coronary syndrome stored as STATA data and calculates PTPs
+#' 
+#' download.file('https://vbn.aau.dk/files/331937080/synthetic_cadptp.dta','synthetic_cadptp.dta')
+#' library(haven)
+#' heart <- read_dta("synthetic_cadptp.dta")
+#' 
+#' Estimae PTP:
+#' cadptp_cta(heart[,'sex'],heart[,'age'],heart[,'symp_gr3'],heart[,'nb_rf_5'],heart[,'calciumscoreagatston']) 
+
+
+cadptp_cta <- function(sex, age , symp_gr3, nb_rf, cacs) {
+  
+  
+  # one hot encoding of symptoms
+  symp_non_anglinal <- ifelse(symp_gr3 == 0, 1, 0)
+  symp_typical <- ifelse(symp_gr3 == 2, 1, 0)
+  
+  # Basic PTP
+  ptp_basic = 1. / (1 + exp(-(
+    -5.3575 + (0.7952 * sex) + (0.061684 * age) + (1.2329 * symp_typical) + (-0.58634 *
+                                                                             symp_non_anglinal) + (-0.0054478 * age * symp_typical)
+  )))
+  
+  ptp <- cbind(ptp_basic )
+  
+  
+  # PTP if Number of risk factors is available
+  if(!missing(nb_rf)) {
+  nb_rf_3 <- (nb_rf >= 0) + (nb_rf >= 2) + (nb_rf >= 4)
+  ptp_rf = 1. / (1 + exp(-(
+    -7.1076  + (1.1332 * sex) + (0.072819 * age) +  (1.4252 * symp_typical) + (-0.55152 *
+                                                                               symp_non_anglinal) + (1.1145 * nb_rf_3) + (-0.005578 * age * symp_typical) + (-0.0079662 *
+                                                                                                                                                             age * nb_rf_3) + (-0.11213 * symp_typical * nb_rf_3) + (-0.18948 * sex * nb_rf_3)
+  )))
+  ptp <- cbind(ptp, ptp_rf)
+  }
+   
+  if(!missing(cacs)) {
+  # PTP if CACS is available
+  cacs_1_9 = cacs >= 1 & cacs < 10
+  cacs_10_99 = cacs >= 10 & cacs < 100
+  cacs_100_399 = cacs >= 100 & cacs < 400
+  cacs_400_999 = cacs >= 400 & cacs < 1000
+  cacs_1000 = cacs >= 1000
+  
+  ptp_cacs = 0.014533 + (ptp_rf * 0.27093) + (cacs_1_9 * 0.047056) + (cacs_10_99 * 0.1189) + (cacs_100_399 * 0.34417)  + (cacs_400_999 * 0.5881) + (cacs_1000 * 0.73892) + (ptp_rf * cacs_1_9 * 0.054423) + (ptp_rf * cacs_10_99 * 0.14347) + (ptp_rf * cacs_100_399 * 0.18439) + (ptp_rf * cacs_400_999 * 0.085774) + (ptp_rf * cacs_1000 * -0.081776)  
+   
+ 
+                                                                                                                                                                                                                                                                                  
+    ptp <- cbind(ptp, ptp_cacs)
+   }  
+ 
+  ptp <- data.frame(ptp)
+  
+  if (ncol(ptp)==1)
+    
+  colnames( ptp) <- c('ptp_basic_cta') 
+  
+  else if (ncol(ptp)==2)
+    
+    colnames( ptp) <- c('ptp_basic_cta','ptp_rf_cta') 
+  
+  else if (ncol(ptp)==3)
+    
+    colnames( ptp) <- c('ptp_basic_cta','ptp_rf_cta','ptp_cacs_cta') 
+  
+  end
+  
+  return(ptp)
+  
+}
